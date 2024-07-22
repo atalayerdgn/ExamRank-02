839ab4ebd7ddce4685d74efaeabfeded1b4d40c9
Atalay Erdoğan <110429589+atalayerdgn@users.noreply.github.com>
2024-07-22 18:45:53 +0300

    Update sort_int_tab.c

--- exam-rank2/Level 3/sort_int_tab/sort_int_tab.c
+++ exam-rank2/Level 3/sort_int_tab/sort_int_tab.c
@@ -15,3 +15,14 @@ void    sort_int_tab(int *tab, int size)
             i++;
     }
 }
+
+
+int	main()
+{
+	int tab[6] = {1, 0, 3, 2, 5, 4};
+	int size = 6;
+
+	ft_sort_int_tab(tab, size);
+	printf("%d, %d, %d, %d, %d, %d", tab[0], tab[1], tab[2], tab[3], tab[4], tab[5]); 
+	return (0);
+}	
